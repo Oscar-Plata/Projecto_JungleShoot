6bb887a68fa4520fd2a860a30165bd95d0ffe145
Oscar L. Plata <lopez.oscar24@uabc.edu.mx>
2022-07-24 18:01:13 -0700

    gd

--- Proyecto_JungleShoot/Assets/Scripts/efectoGhost.cs
+++ Proyecto_JungleShoot/Assets/Scripts/efectoGhost.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class efectoGhost : MonoBehaviour
 {
+    public GameObject Padre;
+
+    public float tiempoVida;
+
+    public Color color;
+
+    private SpriteRenderer sr;
+
+    private SpriteRenderer srOtro;
+
+    private Transform trfOtro;
+
+    public Animation an;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        an = GetComponent<Animation>();
+        srOtro = Padre.GetComponent<SpriteRenderer>();
+        trfOtro = Padre.GetComponent<Transform>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        sr.sprite = srOtro.sprite;
+        sr.color = color;
     }
 }
